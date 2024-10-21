a1aceb77432566db56b75f3b1863d5c3cde8e1a5
victoriaConti <mavictoriaconti@gmail.com>
2024-10-21 19:01:02 -0300

    Agrego ej9.ts a clase47 typescript

--- typescript/clase47/ejercicios/ej9.ts
+++ typescript/clase47/ejercicios/ej9.ts
@@ -9,11 +9,29 @@ vacía o eliminar una canción que no existe.
 
 class Playlist {
     private _nombre:string;
-    private _canciones!: []; 
+    private _canciones!: string[]; 
 
     constructor(nombre:string){
 this._nombre = nombre;
+this._canciones! = [];
     }
-
-    
+public get canciones(): string[]{
+    console.log(`La lista de canciones es: `);    
+    return this._canciones;
 }
+
+public agregarCanciones(cancion:string):void{
+    if(typeof cancion === "string"){
+        this._canciones.push(cancion)
+        console.log(`Se agrego la cancion correctamene.`);
+    }else{
+        console.log("Cancion no valida.");        
+    }
+}
+
+//FALTA HACER ELIMINAR CANCION DE LA LISTA
+}
+
+const miPlaylist = new Playlist("Mis Favoritas");
+miPlaylist.agregarCanciones("Asereje");
+console.log(miPlaylist.canciones);
